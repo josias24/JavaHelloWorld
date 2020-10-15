40e79a8f22faff62825a4062b25ad98c9c523ba9
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-14 23:58:03 -0300

    Se agregan comentarios

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -3,5 +3,11 @@ package helloWorld;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
+        //esto es un comentario de una linea
+        /*esto es
+        un comentario
+        de multiples
+        lineas
+         */
     }
 }
