0e0d5efa28f8cd8aed547c0f987b34cedd07dac6
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-15 01:09:45 -0300

    se agregan operadores matematicos

---
+++ src/helloWorld/EjercicioOperadoresMatematicos.java
@@ -0,0 +1,30 @@
+package helloWorld;
+
+public class EjercicioOperadoresMatematicos {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int suma = a + b;
+        System.out.println(a + "+" + b + "=" + suma);
+
+        int c = 10;
+        int d = 2;
+        int resta = c - d;
+        System.out.println(c + "-" + d + "=" + resta);
+
+        int e = 5;
+        int f = 5;
+        int multiplicacion = e * f;
+        System.out.println(e + "*" + f + "=" + multiplicacion);
+
+        int h = 8;
+        int i = 8;
+        int division = h / i;
+        System.out.println(h + "/" + i + "=" + division);
+
+        int j = 15;
+        int k = 12;
+        int modulo = j % k;
+        System.out.println(j + "%" + k + "=" + modulo);
+    }
+}
