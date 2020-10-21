b4290d804bd2f291bbf88b288b152d8d0b3cac68
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-20 21:23:58 -0300

    se agrega if else

--- src/helloWorld/EjercicioIf.java
+++ src/helloWorld/EjercicioIf.java
@@ -2,22 +2,29 @@ package helloWorld;
 
 public class EjercicioIf {
     public static void main(String[] args) {
-        int x = 10;
+        /*int x = 10;
         if (x >= 10 ){
-            System.out.println("Hola");
+            System.out.println("Hola");*/
+
+        int edad = 18;
+        if (edad >= 16) {
+            System.out.println("sos menor de edad");
+        } else {
+            System.out.println("sos mayor de edad");
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
