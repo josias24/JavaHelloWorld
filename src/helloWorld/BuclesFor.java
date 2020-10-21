bee2eb06ad9862ab1d4f627510d03273704a84d7
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-20 23:39:20 -0300

    se agrega ejemplo de valores pares en for

--- src/helloWorld/BuclesFor.java
+++ src/helloWorld/BuclesFor.java
@@ -2,8 +2,11 @@ package helloWorld;
 
 public class BuclesFor {
     public static void main(String[] args) {
-        for (int x = 1; x <=5; x++){
+        /*for (int x = 1; x <=5; x++){
             System.out.println(x);
-        }
+        }*/
+        for(int x=0; x<=10; x=x+2){
+            System.out.println(x);
+        } /*solo muestra valores pares del 0 al 10*/
     }
 }
