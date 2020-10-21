04ff3f2305ae3f63bcf3dabaa1b81da1981f61b3
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-20 23:24:09 -0300

    se agrega ejercicios con bucle while

---
+++ src/helloWorld/BuclesWhile.java
@@ -0,0 +1,18 @@
+package helloWorld;
+
+public class BuclesWhile {
+    public static void main(String[] args) {
+        /*int x = 3;
+        while (x > 0) {
+            System.out.println(x);
+            x--;
+        }*/
+        int x = 5;
+        while (x < 10)
+        {
+            System.out.println(x);
+            x++;
+        }
+        System.out.println("Repeticion terminada");
+    }
+}
