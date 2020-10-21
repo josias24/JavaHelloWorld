65e9d828a9ffa2bb4897b635b7eb62dc1ebf1020
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-20 21:13:22 -0300

    Ejercicio if simple

---
+++ src/helloWorld/EjercicioIf.java
@@ -0,0 +1,23 @@
+package helloWorld;
+
+public class EjercicioIf {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x >= 10 ){
+            System.out.println("Hola");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
