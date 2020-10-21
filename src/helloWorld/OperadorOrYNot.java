44608b5e5e7e5619e3217cb15c429eedcb81c8ba
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-20 22:10:20 -0300

    se agrega ejercicio con or y not

---
+++ src/helloWorld/OperadorOrYNot.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class OperadorOrYNot {
+    public static void main(String[] args){
+        /*int x = 24, y = 8;
+        if(x >= 24 || y == 10){
+            System.out.println("Bienvenido");*/
+        int edad = 20;
+        if(!(edad > 15)){
+            System.out.println("Sos menor");
+        }else{
+            System.out.println("Bienvenido");
+        }
+    }
+}
