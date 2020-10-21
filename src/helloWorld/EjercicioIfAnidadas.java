d9c40bd1fe0ceb149e36f58b90bdbe02c8a85f73
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-20 21:40:42 -0300

    ejercicio con if anidadas

---
+++ src/helloWorld/EjercicioIfAnidadas.java
@@ -0,0 +1,28 @@
+package helloWorld;
+
+public class EjercicioIfAnidadas {
+    public static void main (String[] args) {
+        int x = 10;
+        if (x > 2) {
+            if (x > 8) {
+                System.out.println("Sos bienvenido");
+            } else {
+                System.out.println("No sos bienvenido");
+            }
+        } else {
+            System.out.println("Error");
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
