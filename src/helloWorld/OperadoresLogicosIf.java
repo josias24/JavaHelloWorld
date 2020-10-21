52c934ce9078f92e8deda0c3787a93227d2bfbd2
Josias Samaniego <josiassamaniego8@gmail.com>
2020-10-20 21:56:42 -0300

    se agrega operadores logicos

---
+++ src/helloWorld/OperadoresLogicosIf.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class OperadoresLogicosIf {
+    public static void main(String[] args){
+        int x = 10, y = 20;
+        if(x == 10 && y == 20){
+            System.out.println("Bienvenido");
+        }
+    }
+}
